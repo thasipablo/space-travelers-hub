356c43aa8f5483bfcf7b6ac9b8b84582c03b3409
thasipablo <thasipablo@gmail.com>
2023-09-06 10:04:22 -0400

    add rocket basic styles

--- src/components/RocketCard.jsx
+++ src/components/RocketCard.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const RocketCard = ({ rocket }) => <div>{rocket.name}</div>;
+const RocketCard = ({ rocket }) => (
+  <div className="rocket-card">
+    <img src={rocket.flickr_images[0]} alt={rocket.name} className="image" />
+    <div>
+      <div className="name">{rocket.name}</div>
+      <div className="description">{rocket.description}</div>
+      <button type="button" className="reservation-btn">
+        Reserve Rocket
+      </button>
+    </div>
+  </div>
+);
 
 RocketCard.propTypes = {
   rocket: PropTypes.shape({
     name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    flickr_images: PropTypes.arrayOf(PropTypes.string).isRequired,
   }).isRequired,
 };
 
