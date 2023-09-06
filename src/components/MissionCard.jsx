e60a93cae0929bceb98f5dc5fb4f37bd4d1b2236
Ulises Cappa <ulisescappa@gmail.com>
2023-09-06 02:19:10 -0300

    Add useSelector to get the missionItems. Add useEffect to dispatch the fetchMission function. Add props to the MissionCard components to obtain the relevant data.

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from 'react'
+import PropTypes from 'prop-types';
 
-const MissionCard = () => (
-  <tr>
-    <td>Thaicom</td>
-    <td>Description Thaicom</td>
+const MissionCard = ({ id, missionName, missionDescription }) => (
+  <tr id={id}>
+    <td>{missionName}</td>
+    <td>{missionDescription}</td>
     <td>NOT A MEMBER</td>
     <td>Join Mission</td>
   </tr>
 );
+
+MissionCard.propTypes = {
+  id: PropTypes.string.isRequired, 
+  missionName: PropTypes.string.isRequired, 
+  missionDescription: PropTypes.string.isRequired, 
+};
 
 export default MissionCard;
