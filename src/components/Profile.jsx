26ca0dc5b2aeaa48a55605dda93c72b266c6d316
Thasi Pablo <39631185+thasipablo@users.noreply.github.com>
2023-09-07 19:25:27 +0100

    create profile components with dummy data

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -17,15 +17,29 @@ const Profile = () => {
     <div className="profile">
       <div className="missions">
         <h2>Missions</h2>
+        <ul className="mission-items">
+          {!bookedRockets.length && (
+            <li className="mission-item">
+              You participate to any mission yet.
+            </li>
+          )}
+          {bookedRockets.map((rocket) => (
+            <li className="mission-item">{rocket.name}</li>
+          ))}
+        </ul>
       </div>
       <div className="rockets">
         <h2>My Rockets</h2>
-        <ul>
+        <ul className="rocket-items">
           {!bookedRockets.length && (
-            <li className="empty-msg">You have booked any rocket yet.</li>
+            <>
+              <li className="rocket-item">You have booked any rocket yet.</li>
+              <li className="rocket-item">You have booked any rocket yet.</li>
+              <li className="rocket-item">You have booked any rocket yet.</li>
+            </>
           )}
           {bookedRockets.map((rocket) => (
-            <li>{rocket.name}</li>
+            <li className="rocket-item">{rocket.name}</li>
           ))}
         </ul>
       </div>
